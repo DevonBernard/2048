102392097601deff36309c8424aa7a412f0c13a6
Devon Bernard <dwbcoding@gmail.com>
2022-01-26 13:27:29 -0500

    feat - made basic flask server with auth endpoint

---
+++ server/run.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
+import requests
+import base64
+import json
+
+tenant=''
+target=''
+tenant_auth_token = ''
+accounts = {}
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+# Login or create account for user
+@app.route('/auth', methods=['POST'])
+def auth_account():
+    address = request.json['address']
+
+    if address in accounts:
+        return jsonify({'success':True}), 200
+
+    payload = {
+        'email': f'demo+{address}@test.com',
+        'password': 'BadPassw0rd!',
+        'persistLogin': True
+    }
+    headers = _create_header(tenant_auth_token)
+
+    resp = requests.post(f'{target}/users/{tenant}/create', json=payload, headers=headers)
+    resp_json = resp.json()
+
+    if resp_json['success']:
+        token = resp_json['result']['token']
+        accounts[address] = {'token': token,'depositAddress':''}
+        with open('accounts.json', 'w') as accounts_file:
+            json.dump(accounts, accounts_file)
+        return jsonify({'success':True}), 200
+
+    return jsonify({'success': False, 'error': 'Failed to login'}), 422
+
+def _create_header(token):
+    return {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer %s' % token
+    }
+
+if __name__ == '__main__':
+    accounts_file = open('accounts.json', "r")
+    accounts = json.loads(accounts_file.read())
+    app.run(port=5002, debug=True)
