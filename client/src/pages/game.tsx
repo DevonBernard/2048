d8a8273c72e3147d1b6bd464144eaecb7a83b071
Devon Bernard <dwbcoding@gmail.com>
2022-01-26 13:36:28 -0500

    feat - added nft loading in client

--- client/src/pages/game.tsx
+++ client/src/pages/game.tsx
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useWallet } from '@solana/wallet-adapter-react';
 
 import Header from '../components/Header';
 import Board from '../components/Board';
+import { apiCall } from '../utils/network';
 
 import BoardSizePicker from '../components/BoardSizePicker';
 import { WalletButton } from '../components/Wallet';
 
 export const GamePage: React.FC = () => {
   const { publicKey } = useWallet();
+  const [nfts, setNfts] = useState([]);
+
+  useEffect(() => {
+    if (publicKey) {
+      apiCall('POST', '/auth', { address: publicKey }).then(authResp => {
+        apiCall('GET', `/users/nfts?address=${publicKey}`).then(
+          (nftResp: any) => {
+            console.log('User NFTs', nftResp);
+            if (nftResp.respJson.success) {
+              setNfts(nftResp.respJson.result);
+              const respPowerUps = nftResp.respJson.result.reduce(
+                (agg: any, nft: any) => {
+                  if (nft.attributes && nft.attributes.powerup) {
+                    agg[nft.attributes.powerup] = true;
+                  }
+                  return agg;
+                },
+                {}
+              );
+            }
+          }
+        );
+      });
+    }
+  }, [publicKey]);
 
   return (
     <div>
